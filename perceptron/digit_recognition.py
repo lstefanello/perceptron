f272c7c0f4b409274ef305a2d9373c17c5c56e70
lstefanello <lincolnstefanello@gmail.com>
2024-02-12 22:26:21 -0500

    Update digit_recognition.py
    The defaults are now sane

--- perceptron/digit_recognition.py
+++ perceptron/digit_recognition.py
@@ -10,14 +10,13 @@ def mnist_load(file, samples):
 def main():
     print("loading data...")
     samples = 10000
-    batch_size = 20
+    batch_size = 32
     train = mnist_load("mnist_train.csv", samples)
     validate = mnist_load("mnist_test.csv", samples)
 
-    restart_params = (.0001, 0.01, 0.01, 2*samples/batch_size) #lower bound, upper bound, decay rate, cycle length.
-    structure = [784, 256, 128, 10, 10]
-    activation_functions = ("elu", "elu", "elu", "softmax")
+    structure = [784, 512, 512, 10, 10]
+    activation_functions = ("relu", "relu", "relu", "softmax")
     network = pc.network(structure, activation_functions, train, validate)
-    network.train(dropout=[.5, .2, 0], beta=0.9, lr_func="warm restarts", lr_params=restart_params, batch_size=batch_size, epochs=10, cost_func="cross entropy")
+    network.train(dropout=[0, 0, 0], beta=0.75, lr_func="const", lr_params=0.1, batch_size=batch_size, epochs=5, cost_func="cross entropy")
 
 main()
